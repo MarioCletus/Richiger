4c31cd4a1ff27dbe495ae718d4a695be85dbcaba
Mariano Pucheta <tantosmarianos@gmail.com>
2022-11-16 14:49:04 -0300

    Correcciones

---
+++ app/src/main/java/ar/com/basculasmagris/richiger/model/database/DescargaDao.kt
@@ -0,0 +1,35 @@
+package ar.com.basculasmagris.richiger.model.database
+
+import androidx.room.*
+import com.basculasmagris.richiger.model.entities.Descarga
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DescargaDao {
+    @Insert
+    suspend fun insertDescarga(descarga: Descarga) : Long
+
+    @Query("SELECT * FROM descarga")
+    fun getAllDescargaList(): Flow<MutableList<Descarga>>
+
+    @Query("SELECT * FROM descarga WHERE archive_date IS NULL OR archive_date = ''")
+    fun getActiveDescargaList(): Flow<List<Descarga>>
+
+    @Query("SELECT * FROM descarga WHERE archive_date IS NULL  OR archive_date = ''")
+    fun getFilteredDescargaList(name: String): Flow<List<Descarga>>
+
+    @Query("SELECT * FROM descarga WHERE id = :id")
+    fun getDescargaById(id: Int): Flow<Descarga>
+
+    @Update
+    suspend fun updateDescarga(descarga: Descarga)
+
+    @Query("UPDATE descarga SET updated_date = :date WHERE id = :id")
+    suspend fun setUpdatedDate(id: Long, date: String)
+
+    @Query("UPDATE descarga SET remote_id = :remoteId WHERE id = :id")
+    suspend fun setUpdatedRemoteId(id: Long, remoteId: Long)
+
+    @Delete
+    suspend fun deleteDescarga(descarga: Descarga)
+}
---
+++ app/src/main/java/ar/com/basculasmagris/richiger/model/database/DescargaRepository.kt
@@ -0,0 +1,40 @@
+package ar.com.basculasmagris.richiger.model.database
+
+import androidx.annotation.WorkerThread
+import com.basculasmagris.richiger.model.entities.Descarga
+import kotlinx.coroutines.flow.Flow
+
+class DescargaRepository (private  val descargaDao: DescargaDao) {
+
+    @WorkerThread
+    suspend fun insertDescargaData(descarga: Descarga) : Long {
+        return descargaDao.insertDescarga(descarga)
+    }
+
+    val allDescargaList: Flow<MutableList<Descarga>> = descargaDao.getAllDescargaList()
+    val activeDescargaList: Flow<List<Descarga>> = descargaDao.getActiveDescargaList()
+
+    fun getDescargaById(id: Int) : Flow<Descarga> = descargaDao.getDescargaById(id)
+    fun getFilteredDescargaList(value: String): Flow<List<Descarga>> = descargaDao.getFilteredDescargaList(value)
+
+
+    @WorkerThread
+    suspend fun updateDescargaData(descarga: Descarga){
+        descargaDao.updateDescarga(descarga)
+    }
+
+    @WorkerThread
+    suspend fun setUpdatedDate(id: Long, date: String){
+        descargaDao.setUpdatedDate(id, date)
+    }
+
+    @WorkerThread
+    suspend fun setUpdatedRemoteId(id: Long, remoteId: Long){
+        descargaDao.setUpdatedRemoteId(id, remoteId)
+    }
+
+    @WorkerThread
+    suspend fun deleteDescargaData(descarga: Descarga){
+        descargaDao.deleteDescarga(descarga)
+    }
+}
